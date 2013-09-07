453479c472acebc907ef761a5654aaacd92e3913
Jeroen Baert <jerbae@gmail.com>
2013-09-07 14:46:40 +0200

    Added checks to see if file exists, modified linux build script, code cleanup

--- src/ooc_svo_builder/tri_tools/include/tri_util.h
+++ src/ooc_svo_builder/tri_tools/include/tri_util.h
@@ -5,6 +5,15 @@
 using namespace std;
 using namespace trimesh;
 
+inline bool file_exists(const std::string& name) {
+    if (FILE *file = fopen(name.c_str(), "r")) {
+        fclose(file);
+        return true;
+    } else {
+        return false;
+    }   
+}
+
 template <typename T>
 struct AABox {
 	T min;
