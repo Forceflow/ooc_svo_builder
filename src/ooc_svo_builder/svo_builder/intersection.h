dda797444a062cd0ad2f89f4176b0028af427e85
Jeroen Baert <jerbae@gmail.com>
2013-11-20 18:31:04 +0100

    Performance optimizations

--- src/ooc_svo_builder/svo_builder/intersection.h
+++ src/ooc_svo_builder/svo_builder/intersection.h
@@ -49,12 +49,7 @@ inline bool isPointinCylinder(const vec3 &point, const Cylinder &cyl){
 
 template <typename T>
 inline bool intersectBoxBox(const AABox<T> &a, const AABox<T> &b){
-	if(a.max[0] < b.min[0]) {return false;}
-	if(a.min[0] > b.max[0]) {return false;}
-	if(a.max[1] < b.min[1]) {return false;}
-	if(a.min[1] > b.max[1]) {return false;}
-	if(a.max[2] < b.min[2]) {return false;}
-	if(a.min[2] > b.max[2]) {return false;}
+	if(a.max[0] < b.min[0] || a.min[0] > b.max[0] || a.max[1] < b.min[1] || a.min[1] > b.max[1] || a.max[2] < b.min[2] || a.min[2] > b.max[2]) {return false;}
 	return true; // intersection or inside
 }
 
